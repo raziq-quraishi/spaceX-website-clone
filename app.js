4bf0df3a1e09c76f32f2d620a0a641b40be07dba
Quraishi <abdulraziqquraishi97@gmail.com>
2022-09-14 07:51:53 +0430

    added new comment to the end of the index.html file

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ menuBtn.addEventListener('click', ()=>{
     sideMenu.classList.toggle('hidden');
 })
 
+
+
+// const str = ["2022-09-13T09:40:00.000000Z","2022-09-13T09:40:00.000000Z",null,"2022-09-13T09:40:00.000000Z"]
+
+// let part = str[2]?.split('T')[0];
+// console.log(part);
